cc3e5f9faa01f95e572f2479708e84e49af093f6
saovalaksao <saovalaksao@gmail.com>
2018-01-12 14:58:38 +0700

    ข้อ1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main () {
+       int arr[10] = {3,2,5,5,6,9,1,3,2,7};
 
-        int arr[10] = {3,2,5,5,6,9,1,3,2,7};
+       int i = 0;
 
         // เขียนโปรแกรมเพื่อแสดงค่าใน arr
 
+        for(i = 0; 1 <10; i++) {
+            cout << arr[i];
+        }
         return 0;
 }
